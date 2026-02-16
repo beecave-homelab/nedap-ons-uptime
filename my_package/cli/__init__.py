2b13aafff875f152344656d8d8a1610f5e55ac8f
elvee <lowie@beecave.nl>
2026-02-16 12:30:45 +0100

    chore 📦: Remove package scaffolding and test files

--- my_package/cli/__init__.py
+++
@@ -1,8 +0,0 @@
-"""CLI subpackage for `my_package`.
-
-Exposes the Typer application and command groupings.
-"""
-
-from .app import app
-
-__all__ = ["app"]
--- my_package/utils/__init__.py
+++
@@ -1,8 +0,0 @@
-"""Utility helpers for `my_package`.
-
-Group reusable, non-IO core utilities here to keep concerns separate.
-"""
-
-from .helpers import greet
-
-__all__ = ["greet"]
